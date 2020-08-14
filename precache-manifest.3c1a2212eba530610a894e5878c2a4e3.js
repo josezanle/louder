d6bae0191609ff4593517e2ccb16ecbfc8510a94
josezanle <63666176+josezanle@users.noreply.github.com>
2020-08-14 14:51:14 -0300

    app hecha con React js , contiene frases de personajes de Breaking Bad

---
+++ precache-manifest.3c1a2212eba530610a894e5878c2a4e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d66aa8225e253a6bc3117a8aa323fbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd60fbabb18c02fc2f23",
+    "url": "/static/css/main.076c19cf.chunk.css"
+  },
+  {
+    "revision": "8ab5cd8ac088f2a53bc2",
+    "url": "/static/js/2.c88e0017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c88e0017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd60fbabb18c02fc2f23",
+    "url": "/static/js/main.1fe3c539.chunk.js"
+  },
+  {
+    "revision": "19f1bba4ab322ccc8c80",
+    "url": "/static/js/runtime-main.014608d3.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
